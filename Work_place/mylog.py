1cfe6d3e10dfc4e8bc735ba2f22217f4eec2a020
Han Kim <hankim8849@gmail.com>
2018-05-28 23:54:16 -0700

    add more argument

--- Work_place/mylog.py
+++ Work_place/mylog.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-def printlog(logfile, search_word):
+def printlog(logfile, search_word, pre_rowcount, next_rowcount):
     f = open(logfile)
     logdata = f.read()
     f.close
@@ -11,13 +11,25 @@ def printlog(logfile, search_word):
         print "-" * 70
         print "Log file: " , logfile
         print "Find this word: ", search_word
-        print index
+#       print index  #number of character not line
         print "-" * 70
 
-        print get_log_data(logdata,index)
+        print get_log_data(logdata, index, pre_rowcount, next_rowcount)
         print "-" * 70
 
-def get_log_data(logdata,start_index) :
+def get_log_data(logdata,start_index, pre_rowcount, next_rowcount) :
     enter_index = max(0,logdata.rfind("\n", 0, start_index ))
+    for i in range(0, pre_rowcount):
+        enter_index= max(0,logdata.rfind("\n", 0, enter_index ))
+
+
     enter_index2 = logdata.find("\n", start_index, len(logdata))
+    for i in range(0, next_rowcount):
+        next_end_index2=logdata.find("\n", enter_index2 + 1, len(logdata))
+        if next_end_index2 == -1 :
+            next_end_index2 = enter_index2
+            break
+        else :
+            enter_index2 = next_end_index2
+
     return logdata[enter_index  : enter_index2]
