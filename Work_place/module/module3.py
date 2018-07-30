9ad3de652ee71717370ee7caf69e2177e039d8d1
Han Kim <hankim8849@gmail.com>
2018-07-29 19:44:23 -0700

    new directory

---
+++ Work_place/module/module3.py
@@ -0,0 +1,13 @@
+#! /usr/local/bin/python3
+#python tutorial  #2 chapter 9 참조
+
+def sum(i,j) :
+	return i + j
+def differ(i,j) :
+	return i - j
+#print(sum(1,2))
+#print(differ(1,2))
+
+if __name__ == "__main__" :
+	print(sum(1,2))
+	print(differ(1,2))
