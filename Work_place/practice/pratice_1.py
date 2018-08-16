dd5af9356e8adaed7ce1659ee1dec125d4857ae1
Han Kim <30737682+hkim123@users.noreply.github.com>
2018-08-16 13:16:34 -0700

    updated

--- Work_place/practice/pratice_1.py
+++ Work_place/practice/pratice_1.py
@@ -5,10 +5,8 @@ import random
 # if __name__ == "__main__":
 #     data = [1,3,5,6,7]
 #     while data:print(pop_list(data))
-n = 0
-while  n < 6 :
-    aa = [8,4,10,4,9]
+aa = [1,3,5,6,7]
+while  aa :
     bb = aa.pop(random.randint(0,len(aa)-1))
     print(bb)
-    print(aa)
-    n = n + 1
+#    print(aa)
