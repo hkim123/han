4fe69eeb0da14b16f9cdcdce5ce72499264668b2
Han Kim <hankim8849@gmail.com>
2018-08-15 21:40:32 -0700

    new file

---
+++ Work_place/practice/pratice_1.py
@@ -0,0 +1,13 @@
+import random
+# def pop_list(data):
+#     n = random.randint(0,len(data)-1)
+#     return data.pop(n)
+# if __name__ == "__main__":
+#     data = [1,3,5,6,7]
+#     while data:print(pop_list(data))
+
+while aa :
+    aa = [8,4,10,4,9]
+    bb = aa.pop(random.randint(0,len(aa)-1))
+#while aa :
+    print(bb)
