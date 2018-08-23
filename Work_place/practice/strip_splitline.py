462fd3149c3a7e2c0744999fb1dce681b9152e80
Han Kim <30737682+hkim123@users.noreply.github.com>
2018-08-22 23:45:13 -0700

    update

--- Work_place/practice/strip_splitline.py
+++ Work_place/practice/strip_splitline.py
@@ -4,3 +4,12 @@ a = 'a,b,c,' \
 b = a.split('\n')  #split('\n')은 enter 에 따라 짤라서 list 형태로 저장한다.
 
 print b
+
+
+
+lines = 'Very good morning Jacob Abraham.\n How are you doing today!'
+for item in lines: #글짜(알파벳 하나하나로 구분
+    print (item)   #알파벳 하나하나 print include space
+
+for item in lines.splitlines():   ## enter 를 기점으로 분류
+    print (item.strip())  # 공백을 없애줌.
