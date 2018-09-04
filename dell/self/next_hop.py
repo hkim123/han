ad09e48682db0735dd4fc2caaf0ad3650177ebe1
Han Kim <30737682+hkim123@users.noreply.github.com>
2018-09-04 13:31:25 -0700

    init

---
+++ dell/self/next_hop.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+from __future__ import print_function
+import commands
+import re
+
+prefix = raw_input("Please enter prefix: ")
+
+out = commands.getoutput('hshell -c "l3 defip show" | grep ' + prefix)
+
+next_hop = re.search('(100*.)',out)
+print(next_hop.group(0))
+
+single_next = commands.getoutput('hshell -c "l3 egress show "' + next_hop.group(0))
+print(single_next)
