eecfc3e8fb16c45650e2195e690891b87234a2ef
Han Kim <30737682+hkim123@users.noreply.github.com>
2018-09-11 22:04:15 -0700

    new file

---
+++ mission/lab/lab02.py
@@ -0,0 +1,55 @@
+'''
+## Question1 ##
+
+print("####Question 1####")
+Cel = input("Enter a degree in Celsius : ")
+
+Fah = (9/5)*eval(Cel) + 32
+
+print(Cel + " Celsinus is" , Fah,"Fahrenheit" )
+
+## Question2 ##
+
+print("####Question 2####")
+InvestAmount = eval(input("Enter investment amount: "))
+Month_Rate = eval(input("Enter annual interest rate : ")) / 1200
+number_month = eval(input("Enter number of years: ")) * 12
+
+Future_Value = int(InvestAmount * (1 + Month_Rate) ** number_month *100) / 100
+
+print("Accumulated value is ", Future_Value)
+
+##Questin 3 ##
+
+print("####Question 3####")
+num = eval(input("Enter a number between 0 and 1000 : "))
+
+num_1 = num % 10
+temp_1 = num // 10
+
+num_2 = temp_1 % 10
+temp_2 = temp_1 // 10
+
+num_3 = temp_2 % 10
+num_4 = temp_2 // 10
+
+sum = num_1 + num_2 + num_3 + num_4
+
+print("The sum of the digit is ", sum)
+'''
+
+##Questin 4 ##
+
+print("####Question 4####")
+
+weight_kilo = eval(input("Enter weight in pound: ")) * 0.45359237
+height_meter = eval(input("Enter height in inches : ")) * 0.0254
+BMI = int(weight_kilo / height_meter ** 2 * 10000) / 10000.0
+BMI_2 = weight_kilo / height_meter ** 2
+BMI_1 = round(BMI_2,4)
+
+print(weight_kilo)
+print(height_meter)
+print("BMI is", BMI)
+print(BMI_1)
+print(BMI_2)
